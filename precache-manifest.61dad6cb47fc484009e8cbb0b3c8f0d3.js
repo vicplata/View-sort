27cda4a4393bffd7719a62b70672e611414a09a5
vicplata <47461430+vicplata@users.noreply.github.com>
2020-12-28 16:26:27 -0600

    first commit

---
+++ precache-manifest.61dad6cb47fc484009e8cbb0b3c8f0d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450d33e86c96dd85f1d01f165b3b8b3b",
+    "url": "/viewsort/index.html"
+  },
+  {
+    "revision": "68752b77bf81bba6477e",
+    "url": "/viewsort/static/css/main.38641f04.chunk.css"
+  },
+  {
+    "revision": "1e1ff69676991bc3e787",
+    "url": "/viewsort/static/js/2.992daeef.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/viewsort/static/js/2.992daeef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68752b77bf81bba6477e",
+    "url": "/viewsort/static/js/main.6a1d53d2.chunk.js"
+  },
+  {
+    "revision": "6fdc145d6ecfb9fcdb58",
+    "url": "/viewsort/static/js/runtime-main.f969e619.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/viewsort/static/media/logo.5d5d9eef.svg"
+  }
+]);
